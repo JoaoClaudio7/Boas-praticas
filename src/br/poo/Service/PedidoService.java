089dab6183b99762424c38bdb27118e211530be4
Jonas Silva <js3172078@gmail.com>
2025-11-08 12:36:16 -0300

    Update PedidoService.java - Remoção do relatorio para seguir o principio S do SOLID

--- src/br/poo/Service/PedidoService.java
+++ src/br/poo/Service/PedidoService.java
@@ -64,55 +64,6 @@ public class PedidoService {
             listarPedido(pedido);
         }
     }
-        public void gerarRelatorioConsolidado() {
-        if (pedidos.isEmpty()) {
-            System.out.println("Nenhuma venda registrada.");
-            return;
-    }
-
-        Map<Integer, ProdutoResumo> consolidado = new HashMap<>();
-        double valorTotalGeral = 0;
-        int totalItensVendidos = 0;
-
-        for (Pedido pedido : pedidos) {
-            for (ItemPedido item : pedido.getItens()) {
-                Produto produto = item.getProduto();
-                int codigo = produto.getCodigo();
-                int qtd = item.getQuantidade();
-                double valor = item.getSubtotal();
-
-                ProdutoResumo resumo = consolidado.getOrDefault(codigo, new ProdutoResumo(produto.getNomeProduto()));
-                resumo.quantidadeVendida += qtd;
-                 resumo.valorTotalVendas += valor;
-                consolidado.put(codigo, resumo);
-
-                valorTotalGeral += valor;
-                totalItensVendidos += qtd;
-        }
-    }
-
-        List<ProdutoResumo> lista = new ArrayList<>(consolidado.values());
-        lista.sort((a, b) -> Integer.compare(b.quantidadeVendida, a.quantidadeVendida));
-
-        StringBuilder relatorio = new StringBuilder();
-        relatorio.append("=== RELATÓRIO CONSOLIDADO DE VENDAS ===\n\n");
-
-        for (ProdutoResumo r : lista) {
-            relatorio.append("Produto: ").append(r.nomeProduto)
-                .append(" | Quantidade Vendida: ").append(r.quantidadeVendida)
-                .append(" | Valor Total: R$ ").append(String.format("%.2f", r.valorTotalVendas))
-                .append("\n");
-    }
-
-        relatorio.append("\nQuantidade total de itens vendidos: ").append(totalItensVendidos)
-                .append("\nValor total das vendas: R$ ").append(String.format("%.2f", valorTotalGeral))
-                .append("\n");
-
-        System.out.println(relatorio.toString());
-
-        src.br.poo.Utils.RelatorioUtils.salvarRelatorio(relatorio.toString());
-
-}
 
     private static class ProdutoResumo {
         String nomeProduto;
